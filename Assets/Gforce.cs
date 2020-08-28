11c1627f2e71e53597dc29cbf283e2ef2d4a73e5
ashawk9897 <asge9897@gmail.com>
2020-08-28 20:06:05 +0530

    added rocket controlls

---
+++ Assets/Gforce.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gforce : MonoBehaviour
+{
+    public Rigidbody rb;
+
+    public static List<Gforce> Attractors;
+    const float G = 200.4f;
+
+    void FixedUpdate()
+    {
+        foreach (Gforce attractor in Attractors)
+        {
+            if (attractor != this)
+                Attract(attractor);
+        }
+    }
+
+    void OnEnable()
+    {
+        if (Attractors == null)
+            Attractors = new List<Gforce>();
+
+        Attractors.Add(this);
+    }
+
+    void OnDisable()
+    {
+        Attractors.Remove(this);
+    }
+
+    void Attract(Gforce anotherBody)
+    {
+        Rigidbody rbToAttract = anotherBody.rb;
+        Vector3 direction= rb.position - rbToAttract.position;
+        float distance = direction.magnitude;
+        if (distance <= 5f)
+        {
+            return;
+        }
+        
+        float forceMagnitude = G * (rb.mass * rbToAttract.mass) / Mathf.Pow(distance, 2);
+        Vector3 force = direction.normalized * forceMagnitude;
+        rbToAttract.AddForce(force);
+        return;
+    }
+}
--- Assets/RocketMotion.cs
+++ Assets/RocketMotion.cs
@@ -4,15 +4,65 @@ using UnityEngine;
 
 public class RocketMotion : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    public Rigidbody rb;
+    private bool moveLeft = false;
+    private bool moveUp = false;
+    private bool moveDown = false;
+    private bool moveRight = false;
+    public float sideForce = 500f;
+    public float upForce = 500f;
+    public float downForce = 500f;
+    private string[] keys = { "a", "d", "s", "w" };
     void Update()
     {
-        
+        if (Input.GetKey(keys[0]))
+        {
+            Debug.Log("a pressed");
+            moveRight = true;
+        }
+        else if (Input.GetKey(keys[1]))
+        {
+            moveLeft = true;
+        }
+        else if (Input.GetKey(keys[2]))
+        {
+            moveUp = true;
+        }
+        else if (Input.GetKey(keys[3]))
+        {
+            moveDown = true;
+        }
+        else
+        {
+            moveLeft = false;
+            moveRight = false;
+            moveDown = false;
+            moveUp = false;
+        }
+    }
+    void FixedUpdate()
+    {
+        //rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+        if (moveLeft)
+        {
+            rb.AddForce(sideForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+        else if (moveRight)
+        {
+            rb.AddForce(-sideForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+        else if (moveUp)
+        {
+            rb.AddForce(0, -upForce * Time.deltaTime, 0, ForceMode.VelocityChange);
+        }
+        else if (moveDown)
+        {
+            rb.AddForce(0, downForce * Time.deltaTime, 0, ForceMode.VelocityChange);
+        }
+
+        //if (rb.position.y < -1f)
+        //{
+        //    FindObjectOfType<GameManager>().EndGame();
+        //}
     }
 }
